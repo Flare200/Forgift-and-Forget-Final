a27fd49139a543ac5ab23264847aae1a55b9cfdc
Aaron R. L. Janushan <35873970+arljanushan@users.noreply.github.com>
2018-02-17 13:14:20 -0800

    Changes to the friend list and overall app style

--- ForgiftandForget/app/src/main/java/cs472/forgiftandforget/friendList.java
+++ ForgiftandForget/app/src/main/java/cs472/forgiftandforget/friendList.java
@@ -2,12 +2,75 @@ package cs472.forgiftandforget;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.ListView;
+import android.widget.SimpleExpandableListAdapter;
 
-public class friendList extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class friendList extends AppCompatActivity
+{
+    ExpandableListView friendList;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friend_list);
+
+        //Super Basic set up
+        friendList = (ExpandableListView) findViewById(R.id.listView);
+
+        HashMap<String,List<String>> ideaList = new HashMap<String,List<String>>();
+
+        String friends[] = getResources().getStringArray(R.array.Testing_list);
+        String sl01[] = getResources().getStringArray(R.array.Fred_list);
+        String sl02[] = getResources().getStringArray(R.array.Grace_list);
+        String sl03[] = getResources().getStringArray(R.array.Bob_list);
+
+        List<String> headings = new ArrayList<String>();
+        List<String> subList01 = new ArrayList<String>();
+        List<String> subList02 = new ArrayList<String>();
+        List<String> subList03 = new ArrayList<String>();
+
+
+        for(String title : friends)
+        {
+            headings.add(title);
+        }
+        for(String title : sl01)
+        {
+            subList01.add(title);
+        }
+        for(String title : sl02)
+        {
+            subList02.add(title);
+        }
+        for(String title : sl03)
+        {
+            subList03.add(title);
+        }
+
+        ideaList.put(headings.get(0),subList01);
+        ideaList.put(headings.get(1),subList02);
+        ideaList.put(headings.get(2),subList03);
+
+        friendsListAdapter myAdapter = new friendsListAdapter(this,headings,ideaList);
+        friendList.setAdapter(myAdapter);
+
+
     }
+
+    private void test()
+    {
+        //ExpandableListView listView = (ExpandableListView) findViewById(R.id.listView);
+        //listView.
+    }
+
 }
