ae9bc7d6bb9bd765eb32e1bbc492808feb977d35
BOX-O-T\Tristan <tristanmoore500@gmail.com>
2018-02-16 21:06:03 -0800

    added friendList table to the dbscript

--- DatabaseInfo/CreateDatabaseTables.sql
+++ DatabaseInfo/CreateDatabaseTables.sql
@@ -1,3 +1,16 @@
+CREATE TABLE friendList
+(
+  friendListID      INT         NULL,
+  friendLoginString VARCHAR(32) NULL
+)
+  ENGINE = InnoDB;
+
+CREATE INDEX friendList_userInfo_friendListID_fk
+  ON friendList (friendListID);
+
+CREATE INDEX friendList_userInfo_loginString_fk
+  ON friendList (friendLoginString);
+
 CREATE TABLE gift
 (
   giftID    INT                    NOT NULL
@@ -60,16 +73,27 @@ CREATE INDEX reminder_userInfo_loginString_fk_2
 
 CREATE TABLE userInfo
 (
-  loginString VARCHAR(32) NOT NULL
+  loginString  VARCHAR(32) NOT NULL
     PRIMARY KEY,
-  userName    VARCHAR(32) NOT NULL,
-  userEmail   VARCHAR(32) NOT NULL,
+  userName     VARCHAR(32) NOT NULL,
+  userEmail    VARCHAR(32) NOT NULL,
+  friendListID INT         NOT NULL,
   CONSTRAINT userInfo_loginString_uindex
   UNIQUE (loginString),
+  CONSTRAINT userInfo_friendListID_uindex
+  UNIQUE (friendListID),
   CONSTRAINT userInfo_loginInfo_loginString_fk
   FOREIGN KEY (loginString) REFERENCES loginInfo (loginString)
 )
   ENGINE = InnoDB;
+
+ALTER TABLE friendList
+  ADD CONSTRAINT friendList_userInfo_friendListID_fk
+FOREIGN KEY (friendListID) REFERENCES userInfo (friendListID);
+
+ALTER TABLE friendList
+  ADD CONSTRAINT friendList_userInfo_loginString_fk
+FOREIGN KEY (friendLoginString) REFERENCES userInfo (loginString);
 
 ALTER TABLE reminder
   ADD CONSTRAINT reminder_userInfo_loginString_fk
@@ -96,5 +120,4 @@ CREATE INDEX userListPair_userInfo_loginString_fk
 
 CREATE INDEX userListPair_userInfo_loginString_fk_2
   ON userListPair (recipLoginString);
-
 
