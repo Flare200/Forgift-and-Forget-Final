515b17b92a66d0a7c44a5ee2e94bb711078d429d
Aaron Janushan <janusha2@ad.unlv.edu>
2018-02-19 20:37:00 -0800

    Stuff for adding users to the main list and some new class to add in creating friends

---
+++ ForgiftandForget/app/src/main/java/cs472/forgiftandforget/entireCreation.java
@@ -0,0 +1,19 @@
+package cs472.forgiftandforget;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class entireCreation extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entire_creation);
+    }
+
+    public void onclick()
+    {
+        
+    }
+}
