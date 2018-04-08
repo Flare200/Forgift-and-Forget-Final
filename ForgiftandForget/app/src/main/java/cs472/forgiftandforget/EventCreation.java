3f3323090b78afb1815784b1cafa478e2e84ee57
Michael-T-Murphy <35878176+Michael-T-Murphy@users.noreply.github.com>
2018-04-08 15:32:05 -0700

    added permission requests.

--- ForgiftandForget/app/src/main/java/cs472/forgiftandforget/EventCreation.java
+++ ForgiftandForget/app/src/main/java/cs472/forgiftandforget/EventCreation.java
@@ -6,11 +6,13 @@ import android.app.TimePickerDialog;
 import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.provider.CalendarContract;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.InputType;
@@ -34,8 +36,9 @@ public class EventCreation extends AppCompatActivity {
 	private String friendID;
 	final static int TIME_PICK = 1;
 	final static int DATE_PICK = 0;
-	final static int MAX_HOURS = 12;
 	final static int TEN = 10;
+	final static int TWELVE = 12;
+	final static int MY_PERMISSIONS_REQUEST_READ_WRITE_CALENDAR = 88;
 	int year;
 	int month;
 	int day;
@@ -44,7 +47,8 @@ public class EventCreation extends AppCompatActivity {
 	Boolean timeSet = false;
 	Boolean dateSet = false;
 	Boolean returningFromCalendar = false;
-	int calendarID = 0;
+	int calendarID = 1;
+
 
 
 	@Override
@@ -57,37 +61,23 @@ public class EventCreation extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_event_creation);
 
-		Uri uri = CalendarContract.Calendars.CONTENT_URI;
-		try {
-			Cursor calendarCursor = managedQuery(uri, null, null, null, null);
-
-			int indexPrimary = calendarCursor.getColumnIndexOrThrow(CalendarContract.Calendars.IS_PRIMARY);
-			int indexID = calendarCursor.getColumnIndexOrThrow(CalendarContract.Calendars._ID);
-			while (calendarCursor.moveToNext()) {
-				if (calendarCursor.getInt(indexPrimary) == 1) {
-					calendarID = calendarCursor.getInt(indexID);
-				}
-			}
-		}catch(Exception e) {
-			// my device(ZTE Axon7) has no isPrimary column in the table, but id of 1 works for me
-			calendarID = 1;
-		}
-
-
 		eventListID = getIntent().getStringExtra("ELID");
 		friendID = getIntent().getStringExtra("FID");
 
 		eventField = (EditText) findViewById(R.id.event);
 		dateField = (EditText) findViewById(R.id.date);
 		timeField = (EditText) findViewById(R.id.time);
+
+		// set calendar to today
 		Calendar cal = Calendar.getInstance();
 		year = cal.get(Calendar.YEAR);
 		month = cal.get(Calendar.MONTH);
 		day = cal.get(Calendar.DAY_OF_MONTH);
 
-
+		// disallow any user input to date and time fields
 		dateField.setInputType(InputType.TYPE_NULL);
 		timeField.setInputType(InputType.TYPE_NULL);
+
 		dateField.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
@@ -116,15 +106,13 @@ public class EventCreation extends AppCompatActivity {
 				}
 			}
 		});
-
-
 	}
 
 	@Override
 	protected Dialog onCreateDialog(int id) {
-		if (id == 0) {
+		if (id == DATE_PICK) {
 			return new DatePickerDialog(this, datePickerListener, year, month, day);
-		} else if (id == 1) {
+		} else if (id == TIME_PICK) {
 			return new TimePickerDialog(this, timePickerListener, hour, minute, false);
 		}
 		return null;
@@ -151,12 +139,15 @@ public class EventCreation extends AppCompatActivity {
 			String amOrPm = "am";
 			String setTime;
 
+			// 12 - 1 am, set up to not show 0 o'clock
 			if (hourFixed == 0) {
-				hourFixed = 12;
-			} else if (hourFixed > 12) {
-				hourFixed -= 12;
+				hourFixed = TWELVE;
+			// military time, set up for standard time
+			} else if (hourFixed > TWELVE) {
+				hourFixed -= TWELVE;
 				amOrPm = "pm";
 			}
+			// set a leading 0 if minutes are less than 10
 			if (minute < TEN) {
 				setTime = hourFixed + ":0" + minute + amOrPm;
 			} else {
@@ -174,47 +165,13 @@ public class EventCreation extends AppCompatActivity {
 		}
 		Event newEvent = new Event(eventField.getText().toString(), dateField.getText().toString());
 		Event.AddEvent(eventListID, friendID, newEvent);
-		Toast.makeText(getApplicationContext(), "Adding " + newEvent.name + " to events", Toast.LENGTH_LONG).show();
-
-		Calendar startTime = Calendar.getInstance();
-		startTime.set(year, month - 1, day, hour, minute);
-		Calendar endTime = Calendar.getInstance();
-		endTime.set(year, month - 1, day, hour, minute);
-
-		returningFromCalendar = true;
-		ContentResolver cr = getContentResolver();
-		ContentValues values = new ContentValues();
-		TimeZone timeZone = TimeZone.getDefault();
-		values.put(CalendarContract.Events.DTSTART, startTime.getTimeInMillis());
-		values.put(CalendarContract.Events.DTEND, endTime.getTimeInMillis());
-		values.put(CalendarContract.Events.EVENT_TIMEZONE, timeZone.getID());
-		values.put(CalendarContract.Events.TITLE, eventField.getText().toString());
-		values.put(CalendarContract.Events.DESCRIPTION, eventField.getText().toString());
-		values.put(CalendarContract.Events.CALENDAR_ID, calendarID);
-		values.put(CalendarContract.Events.HAS_ALARM, 1);
-		Uri uri = cr.insert(CalendarContract.Events.CONTENT_URI, values);
-		String calendarEventID = uri.getLastPathSegment();
-
-
-		Uri REMINDERS_URI = Uri.parse("content://com.android.calendar/reminders");
-		values = new ContentValues();
-		values.put( "event_id", calendarEventID);
-		values.put( "method", 1 );
-		values.put( "minutes", 1 );
-		getContentResolver().insert( REMINDERS_URI, values );
-
-		Intent intent = new Intent(EventCreation.this, FriendList.class);
-		finish();
-		startActivity(intent);
+		checkPermissions();
 	}
-
 
 	@Override
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
 		if (keyCode == KeyEvent.KEYCODE_BACK) {
-			Intent intent = new Intent(EventCreation.this, FriendList.class);
-			finish();
-			startActivity(intent);
+			ReturnToFriendList();
 		}
 		return super.onKeyDown(keyCode, event);
 	}
@@ -222,16 +179,115 @@ public class EventCreation extends AppCompatActivity {
 	@Override
 	protected void onResume() {
 		super.onResume();
-
-		if (returningFromCalendar) {
-			returningFromCalendar = false;
-			ReturnToFriendList();
-		}
 	}
 
 	private void ReturnToFriendList() {
 		Intent intent = new Intent(EventCreation.this, FriendList.class);
 		finish();
 		startActivity(intent);
+	}
+
+	public void addCalendarEntry(){
+		Calendar startTime = Calendar.getInstance();
+		startTime.set(year, month - 1, day, hour, minute);
+		Calendar endTime = Calendar.getInstance();
+		endTime.set(year, month - 1, day, hour, minute);
+		Uri uri = CalendarContract.Calendars.CONTENT_URI;
+		int indexPrimary;
+		int indexID;
+		// check calendar table for the calendar ID of the devices primary calendar, if fails set to 1
+		try {
+			Cursor calendarCursor = managedQuery(uri, null, null, null, null);
+			if (android.os.Build.VERSION.SDK_INT >= 17) {
+				indexPrimary = calendarCursor.getColumnIndexOrThrow(CalendarContract.Calendars.IS_PRIMARY);
+				indexID = calendarCursor.getColumnIndexOrThrow(CalendarContract.Calendars._ID);
+				while (calendarCursor.moveToNext()) {
+					if (calendarCursor.getInt(indexPrimary) == 1) {
+						calendarID = calendarCursor.getInt(indexID);
+					}
+				}
+			}
+		}catch(Exception e) {
+			// my device(ZTE Axon7) has no isPrimary column in the table, but id of 1 works for me
+			calendarID = 1;
+		}
+
+		try {
+			// create and insert calendar entry
+			ContentResolver cr = getContentResolver();
+			ContentValues values = new ContentValues();
+			TimeZone timeZone = TimeZone.getDefault();
+			values.put(CalendarContract.Events.DTSTART, startTime.getTimeInMillis());
+			values.put(CalendarContract.Events.DTEND, endTime.getTimeInMillis());
+			values.put(CalendarContract.Events.EVENT_TIMEZONE, timeZone.getID());
+			values.put(CalendarContract.Events.TITLE, eventField.getText().toString());
+			values.put(CalendarContract.Events.DESCRIPTION, eventField.getText().toString());
+			values.put(CalendarContract.Events.CALENDAR_ID, calendarID);
+			values.put(CalendarContract.Events.HAS_ALARM, 1);
+			Uri eventUri = cr.insert(CalendarContract.Events.CONTENT_URI, values);
+
+			// create and insert event reminder
+			String calendarEventID;
+			if (eventUri != null) {
+				calendarEventID = eventUri.getLastPathSegment();
+				Uri REMINDERS_URI = Uri.parse("content://com.android.calendar/reminders");
+				values = new ContentValues();
+				values.put("event_id", calendarEventID);
+				values.put("method", 1);
+				values.put("minutes", 1);
+				getContentResolver().insert(REMINDERS_URI, values);
+			}
+
+		}catch(SecurityException e){
+			Toast.makeText(getApplicationContext(), "Added " + eventField.getText().toString() + "to events\nUnable to update Calendar", Toast.LENGTH_LONG).show();
+			ReturnToFriendList();
+		}
+		Toast.makeText(getApplicationContext(), "Added " + eventField.getText().toString() + " to events and Calendar", Toast.LENGTH_LONG).show();
+
+		ReturnToFriendList();
+	}
+
+	@Override
+	public void onRequestPermissionsResult(int requestCode,
+	                                       @NonNull String permissions[], @NonNull int[] grantResults) {
+		switch (requestCode) {
+			case MY_PERMISSIONS_REQUEST_READ_WRITE_CALENDAR: {
+				if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+					// permission was granted
+					addCalendarEntry();
+
+				} else {
+					// permission denied, send back to friendsList without adding calendar entry
+					Toast.makeText(getApplicationContext(), "Added " + eventField.getText().toString() + " to events\nUnable to update Calendar", Toast.LENGTH_LONG).show();
+					ReturnToFriendList();
+				}
+				break;
+			}
+		}
+	}
+
+	public void checkPermissions(){
+		boolean write = true;
+		boolean read = true;
+
+
+		// check if permission has already been granted for read and write calendar
+		if (ContextCompat.checkSelfPermission(EventCreation.this, Manifest.permission.WRITE_CALENDAR) != PackageManager.PERMISSION_GRANTED) {
+			write = false;
+		}
+		if(ContextCompat.checkSelfPermission(EventCreation.this, Manifest.permission.READ_CALENDAR) != PackageManager.PERMISSION_GRANTED) {
+			read = false;
+		}
+
+		// check if both are available, if not, request
+		if(!(read && write)) {
+			ActivityCompat.requestPermissions(EventCreation.this,
+					new String[]{Manifest.permission.READ_CALENDAR, Manifest.permission.WRITE_CALENDAR},
+					MY_PERMISSIONS_REQUEST_READ_WRITE_CALENDAR);
+		}else{
+			//permission was already granted
+			addCalendarEntry();
+		}
 	}
 }
